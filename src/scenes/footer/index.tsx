19429e3d98292948823cc6724dff3fa02ab35bfb
Prince Chaudhary <108574095+Princeadhrana@users.noreply.github.com>
2025-03-21 09:39:01 +0530

    Update index.tsx

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -14,15 +14,15 @@ const Footer = () => {
         <div className="mt-16 basis-1/4 md:mt-0">
            
           <h4 className="font-bold">Links</h4>
-          <p className="my-5"><a href="https://www.facebook.com/gaurav.choudhary.7739"  target="_blank">Facebook</a></p>
-          <p className="my-5"><a href="https://www.instagram.com/gaurav.adhrana/"  target="_blank">Instagram</a></p>
+          <p className="my-5"><a href=""  target="_blank">Facebook</a></p>
+          <p className="my-5"><a href=""  target="_blank">Instagram</a></p>
           <p className="my-5"><a href="https://github.com/Princeadhrana"  target="_blank">Github</a></p>
           
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Gaurav Chaudhary</p>
-          <p>+91-8881684734</p>
+          <p className="my-5">Prince Chaudhary</p>
+          <p></p>
         </div>
       </div>
     </footer>
